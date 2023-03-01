df9432f082769a95fe1b0690b14709fa5298aa0f
RinzlerN26 <Email>
2023-03-02 02:08:35 +0530

    Initial Commit

---
+++ lib/Services/global_variables.dart
@@ -0,0 +1,7 @@
+
+String loginUrlImage = 'https://images.unsplash.com/photo-1617526738882-1ea945ce3e56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2095&q=80';
+String signUpUrlImage = 'https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg';
+String forgetUrlImage = 'https://images.pexels.com/photos/9578527/pexels-photo-9578527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+String? name = '';
+String? userImage = '';
+String? location = '';
